f37ff4b52d0efc29f2f539fb84e720ade355a85c
Michael Dautermann <dautermann-github@spamarrest.com>
2014-11-05 04:03:13 -0800

    got the dynamic date plot better figured out...

--- ArticulateStockTracker/AAPLStockPlotItem.m
+++ ArticulateStockTracker/AAPLStockPlotItem.m
@@ -12,6 +12,7 @@ static const double kFrameRate = 5.0;  // frames per second
 
 static const NSUInteger kMaxDataPoints = 15;
 static NSString *const kPlotIdentifier = @"AAPL Stock Plot";
+static NSTimeInterval kOneDay = 24 * 60 * 60;
 
 @interface AAPLStockPlotItem ()
 
@@ -51,7 +52,8 @@ static NSString *const kPlotIdentifier = @"AAPL Stock Plot";
 {
     [self.plotData removeAllObjects];
 
-    // we could load the data from some remote website, but for the purposes of this demo we'll just load the JSON data....
+    // we could load the data from some remote website (and this may be on deck for when I talk to you guys in real-time),
+    // but for the purposes of this demo we'll just load the JSON data that you provided
     NSURL *applStockDataFile = [[NSBundle mainBundle] URLForResource:@"stockprices" withExtension:@"json"];
     NSError *error = nil;
     NSData *applStockData = [[NSData alloc] initWithContentsOfURL:applStockDataFile options:0 error:&error];
@@ -65,7 +67,7 @@ static NSString *const kPlotIdentifier = @"AAPL Stock Plot";
         }
         else
         {
-            NSLog(@"error parsing jason from URL %@ - %@", [applStockDataFile absoluteString], [error localizedDescription]);
+            NSLog(@"error parsing JSON from URL %@ - %@", [applStockDataFile absoluteString], [error localizedDescription]);
         }
     }
     else
@@ -73,6 +75,57 @@ static NSString *const kPlotIdentifier = @"AAPL Stock Plot";
         NSLog(@"error loading data from URL %@ - %@", [applStockDataFile absoluteString], [error localizedDescription]);
     }
 }
+
+- (unsigned int)getLowestPriceBoundary
+{
+    unsigned int minimumPrice = 1000;
+    
+    for(NSDictionary *priceOnADate in self.stockDataArray)
+    {
+        NSString *closingPriceString = priceOnADate[@"close"];
+        double closingPrice = [closingPriceString doubleValue];
+        
+        if(floor(closingPrice) < minimumPrice)
+        {
+            minimumPrice = floor(closingPrice);
+            
+            unsigned int remainder = minimumPrice % 5;
+            
+            if(remainder != 0)
+            {
+                minimumPrice = (minimumPrice - remainder);
+            }
+        }
+    }
+    return minimumPrice;
+}
+
+- (unsigned int)getYAxisRange
+{
+    unsigned int minimumPrice = [self getLowestPriceBoundary];
+    unsigned int maximumPrice = 0;
+    
+    for(NSDictionary *priceOnADate in self.stockDataArray)
+    {
+        NSString *closingPriceString = priceOnADate[@"close"];
+        double closingPrice = [closingPriceString doubleValue];
+        
+        if(ceil(closingPrice) > maximumPrice)
+        {
+            maximumPrice = ceil(closingPrice);
+            
+            unsigned int remainder = maximumPrice % 5;
+            
+            if(remainder != 0)
+            {
+                maximumPrice = (maximumPrice + remainder);
+            }
+        }
+    }
+
+    return(maximumPrice - minimumPrice) + 1;
+}
+
 
 - (void)renderInLayer:(CPTGraphHostingView *)layerHostingView withTheme:(CPTTheme *)theme animated:(BOOL)animated
 {
@@ -85,8 +138,8 @@ static NSString *const kPlotIdentifier = @"AAPL Stock Plot";
     [self setTitleDefaultsForGraph:graph withBounds:bounds];
     [self setPaddingDefaultsForGraph:graph withBounds:bounds];
 
-    graph.plotAreaFrame.paddingTop    = 15.0;
-    graph.plotAreaFrame.paddingRight  = 15.0;
+    graph.plotAreaFrame.paddingTop    = 75.0;
+    graph.plotAreaFrame.paddingRight  = 75.0;
     graph.plotAreaFrame.paddingBottom = 105.0;
     graph.plotAreaFrame.paddingLeft   = 105.0;
     graph.plotAreaFrame.masksToBorder = NO;
@@ -102,13 +155,15 @@ static NSString *const kPlotIdentifier = @"AAPL Stock Plot";
 
     // Plot space
     CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *)graph.defaultPlotSpace;
+    
+    graph.title = @"Apple Stock Price";
 
     // Axes
     // X axis
-#if 0
-    // this is the x-axis plotting & labeling scheme I'd ***really*** love to use, but I haven't figured out the
-    // orthogonal & interval magic between the X & Y axis, so I'm cheating below by using custom labels.
-    // I'm sorry you have to see it like this.
+    
+#define DIRECTDATE
+    
+#ifdef DIRECTDATE
     NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
 
     [dateComponents setMonth:9];
@@ -118,20 +173,17 @@ static NSString *const kPlotIdentifier = @"AAPL Stock Plot";
     [dateComponents setMinute:0];
     [dateComponents setSecond:0];
 
-    NSCalendar *gregorian = [[NSCalendar alloc]
-                             initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
     NSDate *refDate = [gregorian dateFromComponents:dateComponents];
 
-    NSTimeInterval oneDay = 24 * 60 * 60;
+    NSTimeInterval xLow             = 0.0;
 
-    NSTimeInterval xLow       = 0.0;
-
-    plotSpace.xRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromDouble(xLow) length:CPTDecimalFromDouble(oneDay * 5.0)];
+    plotSpace.xRange                = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromDouble(xLow) length:CPTDecimalFromDouble(kOneDay * 4.0)];
 
     CPTXYAxisSet *axisSet           = (CPTXYAxisSet *)graph.axisSet;
     CPTXYAxis *x                    = axisSet.xAxis;
     x.title                         = @"Date";
-    x.majorIntervalLength           = CPTDecimalFromDouble(oneDay);
+    x.majorIntervalLength           = CPTDecimalFromDouble(kOneDay);
     x.majorGridLineStyle            = majorGridLineStyle;
     x.minorGridLineStyle            = minorGridLineStyle;
     x.orthogonalCoordinateDecimal   = CPTDecimalFromUnsignedInteger(90);
@@ -141,12 +193,10 @@ static NSString *const kPlotIdentifier = @"AAPL Stock Plot";
     CPTTimeFormatter *timeFormatter = [[CPTTimeFormatter alloc] initWithDateFormatter:dateFormatter];
     timeFormatter.referenceDate     = refDate;
     x.labelFormatter                = timeFormatter;
-    // Rotate the labels by 45 degrees, just to show it can be done.
-    //x.labelRotation                 = CPTFloat(M_PI_4);
 
-#endif
-
-#if 1
+#else
+    
+    // if I wanted to cheat by using custom labels
     plotSpace.xRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromUnsignedInteger(0) length:CPTDecimalFromUnsignedInteger(5)];
     CPTXYAxisSet *axisSet = (CPTXYAxisSet *)graph.axisSet;
     CPTXYAxis *x = axisSet.xAxis;
@@ -161,8 +211,7 @@ static NSString *const kPlotIdentifier = @"AAPL Stock Plot";
     // starts the X axis at the low end of the stock range
     x.orthogonalCoordinateDecimal = CPTDecimalFromUnsignedInteger(90);
     x.title = @"Date";
-    //x.titleOffset=47.0f;
-    // x.labelRotation=M_PI/4;
+
     x.labelingPolicy = CPTAxisLabelingPolicyNone;
     NSArray *xAxisLabels = [NSArray arrayWithObjects:@"9/8/2014", @"9/9/2014", @"9/10/2014", @"9/11/2014", @"9/12/2014", nil];
     NSUInteger labelLocation = 0;
@@ -181,18 +230,21 @@ static NSString *const kPlotIdentifier = @"AAPL Stock Plot";
 #endif
 
     // find minimum and maximum possible values for the stock price in our data set
-    plotSpace.yRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromUnsignedInteger(90) length:CPTDecimalFromUnsignedInteger(110 - 90)];
+    plotSpace.yRange = [CPTPlotRange plotRangeWithLocation:CPTDecimalFromUnsignedInteger([self getLowestPriceBoundary]) length:CPTDecimalFromUnsignedInteger([self getYAxisRange])];
 
     // Y axis
+    
+    // first set a number formatter to the prices along the Y axis
     NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
     [numberFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
     [numberFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    
     CPTXYAxis *y                  = axisSet.yAxis;
     y.labelingPolicy              = CPTAxisLabelingPolicyAutomatic;
     y.orthogonalCoordinateDecimal = CPTDecimalFromUnsignedInteger(0);
     y.majorGridLineStyle          = majorGridLineStyle;
     y.minorGridLineStyle          = minorGridLineStyle;
-    y.minorTicksPerInterval       = 3;
+    y.minorTicksPerInterval       = 5;
     y.labelOffset                 = 5.0;
     y.title                       = @"Stock Price";
     y.titleOffset                 = 70.0;
@@ -264,7 +316,7 @@ static NSString *const kPlotIdentifier = @"AAPL Stock Plot";
                   toPlotRange:newRange
                      duration:CPTFloat(1.0 / kFrameRate)];
 
-        NSDictionary *priceOnADate = [self.stockDataArray objectAtIndex:self.currentIndex++];
+        NSDictionary *priceOnADate = [self.stockDataArray objectAtIndex:self.currentIndex];
         NSString *closingPriceString = priceOnADate[@"close"];
         double closingPrice = [closingPriceString doubleValue];
 
@@ -289,7 +341,11 @@ static NSString *const kPlotIdentifier = @"AAPL Stock Plot";
     {
         switch (fieldEnum) {
             case CPTScatterPlotFieldX:
+#ifdef DIRECTDATE
+                num = @(self.currentIndex++ * kOneDay);
+#else
                 num = @(index + self.currentIndex - self.plotData.count);
+#endif
                 break;
 
             case CPTScatterPlotFieldY:
